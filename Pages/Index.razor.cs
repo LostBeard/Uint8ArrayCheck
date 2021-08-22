cde4ae14dd33042a16f9b891783af35f6275e562
TJ <TJ@NV1080>
2021-08-22 10:39:25 -0400

    Add project files.

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Uint8ArrayCheck.Pages;
+public class IndexBase : ComponentBase
+{
+    [Inject]
+    IJSRuntime js { get; set; }
+    protected override void OnAfterRender(bool firstRender)
+    {
+        base.OnAfterRender(firstRender);
+        if (firstRender)
+        {
+            var byteArray = new byte[] { 1, 3, 5  };
+            js.InvokeVoidAsync("uint8arrayTest", byteArray);
+        }
+    }
+}
